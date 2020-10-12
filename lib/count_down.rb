7a1118851c0ece524206531c5eb0651a04ece933
Sin Wildy <sw.wildy@gmail.com>
2020-10-12 03:33:51 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,9 @@
 # Write your code here
+
+magic_exit_number = 7
+count = 0 
+while count < 10 do
+  break if count = magic_exit_number
+  puts "i am the #{count}, I love to count!"
+  count = count + 1 
+end
