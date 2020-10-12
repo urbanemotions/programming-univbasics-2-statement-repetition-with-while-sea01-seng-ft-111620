bf9184194c6f16ef890939a1007d9e67db5b1c51
Sin Wildy <sw.wildy@gmail.com>
2020-10-12 03:54:32 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,27 @@
 # Write your code here
+
+count = 10 
+while count >= 1 do 
+  puts count
+  count -= 1 
+end 
+puts "Happy New Year!"
+
+
+
+#magic_exit_number = 7
+#count = 0
+#while count < 10 && count != magic_exit_number do
+#  puts "I am the #{count}, I love to count!"
+#  count = count + 1 
+#end
+
+
+
+#magic_exit_number = 7
+#count = 0 
+#while count < 10 do
+#  break if count == magic_exit_number
+#  puts "i am the #{count}, I love to count!"
+#  count = count + 1 
+#end
