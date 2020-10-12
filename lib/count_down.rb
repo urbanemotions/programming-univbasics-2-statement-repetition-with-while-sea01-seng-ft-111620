f71e54ace1ced85e3e58d1c5787f909b9d886945
Sin Wildy <sw.wildy@gmail.com>
2020-10-12 03:39:16 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,18 @@
 # Write your code here
 
 magic_exit_number = 7
-count = 0 
-while count < 10 do
-  break if count == magic_exit_number
-  puts "i am the #{count}, I love to count!"
+count = 10
+while count < 10 && count != magic_exit_number do
+  puts "I am the #{count}, I love to count!"
   count = count + 1 
 end
+
+
+
+#magic_exit_number = 7
+#count = 0 
+#while count < 10 do
+#  break if count == magic_exit_number
+#  puts "i am the #{count}, I love to count!"
+#  count = count + 1 
+#end
